c725a977bcc7c6001b0f1d9e4ddd32e3a4f67845
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-06 16:55:37 +0300

    Create More_Free_providers.java

---
+++ More_Free_providers.java
@@ -0,0 +1,40 @@
+Sure, let's go through the features and limitations of the free plans for GitHub Pages and Glitch.
+
+**GitHub Pages:**
+
+GitHub Pages is a static site hosting service that takes HTML, CSS, and JavaScript files straight from a repository on GitHub, optionally runs the files through a build process, and publishes a website.
+
+Features of the free plan include:
+
+- Website may be up to 1GB in size.
+- Bandwidth: 100GB per month.
+- You can set your own domain name for your website via CNAME.
+- There is support for HTTPS for the website. 
+
+Limitations:
+
+- Since GitHub Pages is designed to host your personal, organization, or project pages from a GitHub repository, it doesn't support server-side code such as PHP, Ruby, or Python.
+- It's not designed for high traffic volumes so if your site has a lot of traffic, you may want to consider other options.
+- The customization options are quite limited compared to other hosting services.
+
+[Source](https://www.bitsandpieces.io/15-free-hosting-providers-for-web-developers/)
+
+**Glitch:**
+
+Glitch is a simple tool for creating web apps. It's designed for creating and sharing your web development projects.
+
+Features of the free plan include:
+
+- Provides 4000 requests/hour.
+- The server processes 512MB of RAM.
+- Storage capacity 512MB.
+- Unlimited static sites.
+- Support for import/export GitHub function.
+
+Limitations:
+
+- Your code and project will automatically be displayed as public by default. This might be a concern if you want to keep your code private.
+- Glitch apps go to sleep after 5 minutes of being idle, and wake up when accessed which can lead to a slight delay when accessing the site after it has been idle.
+- Glitch is not designed for large-scale, high-traffic applications.
+
+[Source](https://www.bitsandpieces.io/15-free-hosting-providers-for-web-developers/)
