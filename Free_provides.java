51603c762d264aecfb82312a19335ae85ab3d267
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-06 16:54:45 +0300

    Create Free_provides.java

---
+++ Free_provides.java
@@ -0,0 +1,119 @@
+Certainly! Here's an overview of the free hosting options mentioned in the article, along with their key features and limitations:
+
+1. **GitHub Pages:**
+   - **Features:**
+     - Website up to 1GB in size.
+     - 100GB per month bandwidth.
+     - Custom domain via CNAME.
+     - HTTPS support.
+
+2. **Glitch:**
+   - **Features:**
+     - 4000 requests/hour.
+     - 512MB of RAM.
+     - 512MB storage.
+     - Unlimited static sites.
+   - **Cons:** Projects are public by default.
+
+3. **Fleek:**
+   - **Features:**
+     - Editable domain.
+     - HTTPS support.
+     - 3GB storage.
+     - Unlimited websites.
+     - Git-based deployment.
+     - 250 minutes to build and 50GB bandwidth.
+     - Free SSL.
+
+4. **Netlify:**
+   - **Features:**
+     - Git-based automatic builds.
+     - Global edge network deployment.
+     - 100GB/month bandwidth.
+     - Unlimited websites.
+     - Version rollback.
+     - Free CLI.
+
+5. **Render:**
+   - **Features:**
+     - 100GB/month bandwidth and storage.
+     - Fast CDN.
+     - Custom domain with SSL.
+     - Git-based deployment.
+
+6. **Surge:**
+   - **Features:**
+     - 404.html page functionality.
+     - Unlimited websites and APIs.
+     - Custom domains.
+     - Easy CLI deployment.
+
+7. **Vercel:**
+   - **Features:**
+     - Edge network for performance.
+     - Unlimited websites and APIs.
+     - Custom domains with SSL.
+     - Serverless Functions.
+     - Image optimization (limited for free).
+
+8. **Hostman:**
+   - **Features:**
+     - Create 3 websites.
+     - 100GB/month bandwidth.
+     - Git-based automatic builds.
+     - Free SSL certificate.
+     - CDN deployment.
+     - Version rollbacks.
+
+9. **Cloudflare Pages:**
+   - **Features:**
+     - Git integration.
+     - Unlimited websites and pages.
+     - No bandwidth limits.
+     - CDN integration.
+     - 500 builds/month.
+
+10. **W3Schools Spaces:**
+    - **Features:**
+      - Free templates.
+      - 500 requests/month.
+      - 100MB data and storage/month.
+      - Free SSL.
+
+11. **Heroku:**
+    - **Features:**
+      - Custom domains.
+      - Git and Docker deployment.
+      - CLI support.
+    - **Cons:** Apps may sleep if inactive for 30 minutes.
+
+12. **Firebase:**
+    - **Features:**
+      - 10GB storage.
+      - 360MB bandwidth/day.
+      - Custom domain.
+      - Support for multiple pages.
+      - SSL.
+
+13. **Digital Ocean:**
+    - **Features:**
+      - Deploy from GitHub or GitLab.
+      - Free CDN.
+      - Custom domains.
+      - 1GB bandwidth/project.
+      - SSL/HTTPS support.
+
+14. **GitLab Pages:**
+    - **Features:**
+      - Static Site Generators (SSG) support.
+      - CLI.
+      - Unlimited websites.
+      - Free SSL/HTTPS.
+
+15. **Amazon S3:**
+    - **Features:**
+      - 5GB storage.
+      - 20,000 requests for websites.
+      - 2,000 Put requests.
+
+Each of these hosting providers offers a set of features that can be beneficial for different types of web development projects. The limitations are usually related to storage, bandwidth, and sometimes the visibility of your project to the public. It's important to carefully consider your project's requirements and choose a hosting provider that aligns with your needs. Additionally, some providers may offer premium plans with more resources and features if you outgrow their free tier.
